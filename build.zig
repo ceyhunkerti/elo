2d7a0d88909dc3953a1bc7ec987d805201789d7a
Ceyhun <ceyhun.kerti@gmail.com>
2025-01-25 19:19:46 +0300

    init build package

--- build.zig
+++ build.zig
@@ -2,22 +2,10 @@ const std = @import("std");
 const builtin = @import("builtin");
 const Step = std.Build.Step;
 
-comptime {
-    // This is the required Zig version for building this project. We allow
-    // any patch version but the major and minor must match exactly.
-    const required_zig = "0.13.0";
+const buildpkg = @import("src/build/main.zig");
 
-    // Fail compilation if the current Zig version doesn't meet requirements.
-    const current_vsn = builtin.zig_version;
-    const required_vsn = std.SemanticVersion.parse(required_zig) catch unreachable;
-    if (current_vsn.major != required_vsn.major or
-        current_vsn.minor != required_vsn.minor)
-    {
-        @compileError(std.fmt.comptimePrint(
-            "Your Zig version v{} does not meet the required build version of v{}",
-            .{ current_vsn, required_vsn },
-        ));
-    }
+comptime {
+    buildpkg.requireZig("0.13.0");
 }
 
 // Although this function looks imperative, note that its job is to
