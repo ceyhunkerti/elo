92f48c7f622a1dc0b7efd9833bd925fcd12692da
Ceyhun <ceyhun.kerti@gmail.com>
2025-02-15 19:59:06 +0300

    examples

--- exmaples/scripts/pg-to-ora.sh
+++ exmaples/scripts/pg-to-ora.sh
@@ -5,9 +5,9 @@
     --source-password $PG_TEST_PASSWORD \
     --source-database $PG_TEST_DATABASE \
     --source-host $PG_TEST_HOST \
-    --source-sql "select * from test_pg_to_ora_02" \
+    --source-sql "select * from ora_to_pg_01 order by a" \
     --sink oracle \
     --sink-username $ORACLE_TEST_USERNAME \
     --sink-password $ORACLE_TEST_PASSWORD \
     --sink-connection-string $ORACLE_TEST_CONNECTION_STRING \
-    --sink-table test_pg_to_ora_01
+    --sink-table pg_to_ora_02
